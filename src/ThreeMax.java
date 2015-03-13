f02b31de56f935766c049e59e7522abdb317ebd3
jjzhai <jjzhai@lisystec.com>
2015-03-13 18:08:07 +0800

    second commit

--- src/ThreeMax.java
+++ src/ThreeMax.java
@@ -1,10 +1,16 @@
+import java.awt.Component;
+
 import javax.swing.JFrame;
+import javax.swing.JTextPane;
 
 
 public class ThreeMax {
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("求最大值");
 		frame.setSize(500, 500);
+		JTextPane textPane = new JTextPane();
+		textPane.setSize(15, 10);
+		frame.add(textPane);
 		frame.setVisible(true);
 	}
 }
