4e23e6eed5cc26a49abe0b0ac4b8747a13578eca
jjzhai <jjzhai@lisystec.com>
2015-03-13 17:53:53 +0800

    first commit

---
+++ src/ThreeMax.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+
+public class ThreeMax {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("求最大值");
+		frame.setSize(500, 500);
+		frame.setVisible(true);
+	}
+}
